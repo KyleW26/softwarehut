c48a77eab46ea35fddd794e9e6de0c7ec9a3afb4
Kyle Williams <33125320+KyleW26@users.noreply.github.com>
2018-05-02 15:43:13 +0100

    Changed variable names

--- Combined HTML/setup/create_db.php
+++ Combined HTML/setup/create_db.php
@@ -1,7 +1,7 @@
 <?php
 	if (!empty($_POST["setup_key"]) && $_POST["setup_key"] == "gc3thI7PuUNY1l3p") {
 		require (__DIR__ . "/../login/user_system.php");
-		$query = "CREATE DATABASE IF NOT EXISTS gholdswo_project2;";
+		$query = "CREATE DATABASE IF NOT EXISTS $DB_name;";
 		$conn = mysqli_connect($DB_server_name, $DB_username, $DB_password);
 		if(!$conn){
 			die('Could not connect: ' . mysqli_error($conn));
@@ -11,7 +11,7 @@
 			die("Error: " . $query . "" . mysqli_error($conn));
 		}
 
-		$query = "USE `gholdswo_project2`;";
+		$query = "USE `$DB_name`;";
 
 		if (!mysqli_query($conn, $query)) {
 			die("Error: " . $query . "" . mysqli_error($conn));
